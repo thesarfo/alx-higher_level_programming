4d39b90660a6309c785dca098125242f7025121d
thesarfo <ernestsarfo100@gmail.com>
2023-06-06 13:47:43 +0000

    fix for 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 '''print numbers in the range in dec and hexadec'''
 for num in range(0, 99):
-    print("{} = {}".format(num, hex(number)))
+    print("{} = {}".format(num, hex(num)))
