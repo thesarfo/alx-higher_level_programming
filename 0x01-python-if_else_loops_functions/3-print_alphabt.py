803d4dad347c329be2219e81ab2cbe0ca7a008c8
thesarfo <ernestsarfo100@gmail.com>
2023-06-06 10:46:47 +0000

    fourth to sixth tasks

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for ascii_val in range(97, 123):
+    if ascii_val == 113 or ascii_val == 101:
+        continue
+    print(chr(ascii_val), end="")                                         
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+	print(f"{number:02d}", end=", ")
+print()
