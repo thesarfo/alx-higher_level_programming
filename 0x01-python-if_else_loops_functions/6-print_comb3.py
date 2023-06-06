0b340ead10b9bb77144edc857b2cf197e300bb27
thesarfo <ernestsarfo100@gmail.com>
2023-06-06 13:51:06 +0000

    fix for 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for dig in range(10):
-	for dig1 in range(dig + 1, 10):
-		print(f"{digit1}{digit2}", end=", ")
+    for dig1 in range(dig + 1, 10):
+        print(f"{digit1}{digit2}", end=", ")
 print()
