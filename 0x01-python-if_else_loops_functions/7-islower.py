0c6dcd2365d78a0e0c0224f1e69f79e62858e98c
thesarfo <ernestsarfo100@gmail.com>
2023-06-06 14:08:00 +0000

    fix for 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    ascii_val = ord(c)
-    if ascii_val >= 97 and ascii_val <= 122:
-        return True
-    else:
-        return False
+    for n in range(ord('a'), ord('{')):
+        if n == ord(c):
+            return True
+    return False
