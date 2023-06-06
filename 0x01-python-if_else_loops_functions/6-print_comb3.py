4cb8a1e58a13bbc6c4562d65061991626af8b83b
thesarfo <ernestsarfo100@gmail.com>
2023-06-06 13:54:39 +0000

    fix for 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for dig in range(10):
     for dig1 in range(dig + 1, 10):
-        print(f"{digit1}{digit2}", end=", ")
-print()
+        if i != 8 or i != 9:
+	    print("{:n}{:n}".format(i, dig1), end=", ")
+print("{:n}{:n}".format(i, dig1))
