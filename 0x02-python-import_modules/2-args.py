4bfc01aae6a17643f4befb0c366539e100becdd9
thesarfo <ernestsarfo100@gmail.com>
2023-06-08 20:58:57 +0000

    added advanced tasks and fixed basic tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
+if __name__ == "__main__":
     import sys
-    args = sys.argv[1:]
-    num_args = len(args)
-    if num_args == 0:
-        print("No arguments were passed")
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
     else:
-        for i in range(num_args):
-            print(i + 1, ":", args[i])
-            print("")
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        print("{:d}: {:s}".format(i, sys.argv[i]))
