10cb4153bc9cb749d21cb83ceda8da41595cd46e
thesarfo <ernestsarfo100@gmail.com>
2023-06-10 06:13:40 +0000

    re sub for task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    '''replaces an element in a list at a specific position
-    without modifying the original list'''
+    '''replaces an element in a list at a specific position  without modifying the original list'''
     my_list_copy =  my_list[:]
     if idx < 0 or idx >= len(my_list):
         return my_list_copy
