c4b60fc2e2a9a4fd9ad8c2f5b60ce00c92b0cc50
thesarfo <ernestsarfo100@gmail.com>
2023-06-25 07:04:43 +0000

    first three tasks, waiting for the checker

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            if (type(my_list[i]) == int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            else:
+                continue
+        print("")
+        return count
+    except ValueError:
+        print("")
+        return count
